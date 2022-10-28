fd48d84c3b7ee6ec3c07e3115808157922106122
Han kyung Sung <hankyungsung@Hans-MBP.lan>
2022-10-27 23:40:06 -0700

    Added eslint for typescript.

---
+++ invitation-backend/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: [
+    "eslint:recommended", "plugin:@typescript-eslint/recommended"
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "linebreak-style": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
