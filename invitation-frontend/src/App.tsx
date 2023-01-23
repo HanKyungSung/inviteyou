0b7f3f84e3fd8c970a54eb0d75b2daab12bf1a7f
Han kyung Sung <hankyungsung@Hans-MBP.lan>
2023-01-23 00:18:59 -0800

    Added simple logic to manually handle sub-domain.

--- invitation-frontend/src/App.tsx
+++ invitation-frontend/src/App.tsx
@@ -5,15 +5,34 @@ import Landing from './component/Landing';
 import Login from './component/Login';
 import Register from './component/Register';
 import Test from './component/Test';
+import * as Constants from './utils/Constants';
 
 function App() {
+  const { NODE_ENV } = process.env;
+  const isDevelopment = NODE_ENV === 'development';
+  const hostname = isDevelopment ? Constants.localhostUrl : Constants.prodhostUrl;
+  const hostnames = isDevelopment ? Constants.localhostUrls : Constants.prodhostUrls;
+  
+  console.log('location', location);
+
+  const subdomain = window.location.host.split('.')[0];
+
+  // Checking this should be dynamic. Need to save it in the database.
+  if(subdomain === 'we') {
+    return (
+      <Test />
+    )
+  } else if(!hostnames.includes(window.location.hostname)) {
+    // If subdomain doesn't match to anything, we reset the url.
+    window.location.replace(hostname);
+  }
+
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/builder" element={<CardBuilder />} />
       <Route path="/Login" element={<Login />} />
       <Route path="/Register" element={<Register />} />
-      <Route path="/Test" element={<Test />} />
     </Routes>
   );
 }
---
+++ invitation-frontend/src/utils/Constants.tsx
@@ -0,0 +1,5 @@
+export const localhostUrl = 'http://localhost:3000';
+export const localhostUrls = ['www.localhost', 'localhost'];
+
+export const prodhostUrl = 'https://inviteyou.ca';
+export const prodhostUrls = ['www.inviteyou.ca', 'inviteyou.ca'];
