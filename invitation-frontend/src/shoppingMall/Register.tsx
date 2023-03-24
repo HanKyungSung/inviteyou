1ff2b61e255ce39bb1f59670cb9b5c2681a72ce0
Hailey Yang <yanghyeri73@gmail.com>
2023-03-23 22:57:27 -0700

    Register Page (initial)

--- invitation-frontend/src/shoppingMall/Register.tsx
+++ invitation-frontend/src/shoppingMall/Register.tsx
@@ -1,32 +1,25 @@
 import {
-  Header,
-  Grid,
   Title,
-  Text,
   Container,
   createStyles,
   Button,
-  Group,
   Box,
   Anchor,
   Stack,
   MantineProvider,
-  Autocomplete,
-  Loader,
   PasswordInput,
   Input,
+  TextInput,
   Checkbox
 } from '@mantine/core';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { IconEyeCheck, IconEyeOff } from '@tabler/icons';
+
 import LandingHeader from '../common/LandingHeader';
 import LandingFooter from '../common/LandingFooter';
 
-
-import { useState, useRef } from 'react';
-
-const SECONDARY_COLOR = '#191F28';
-const COLOR_BLACK = '#222222';
-const STYLE_BTN_COLOR = { from: '#ed6ea0', to: '#ec8c69', deg: 35 };
+import * as ConstantStyle from '../common/Constant';
 
 const useStyles = createStyles((_theme, _params, _getRef) => ({
   loginIcons: {
@@ -43,33 +36,72 @@ const useStyles = createStyles((_theme, _params, _getRef) => ({
   }
 }));
 
+const defaultRegisterData = {
+  name : "",
+  email : "",
+  password : "",
+  password2 : "",
+  checkbox1 : false,
+  checkbox2 : false
+}
+
+const defaultRegisterErrorMessage: Record<string, string> = {
+  name: '',
+  email: '',
+  password: '',
+  password2: '',
+  checkbox1 : '',
+  checkbox2 : ''
+}
+
 const Login = () => {
+  //Email regit
+  //password length : 8 above 16 below
+  //checkbox 
+  //name = first name/ last name
   const { classes } = useStyles();
+  const [form, setForm] = useState(defaultRegisterData)
+  const [errorMessages, setErrorMessages] = useState(defaultRegisterErrorMessage);
+  
+  const handleSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log('submit', form)
+  
+    const { name, email, password, password2, checkbox1, checkbox2 } = form;
+    if (!name || !email || !password || !password2) {
+      const emptyInputError: Record<string, string> = {};
+      Object.entries(form).forEach(([key, value]) => {
+        if (!value) {
+          emptyInputError[key] = `Please enter ${key}`;
+        }
 
-  const timeoutRef = useRef<number>(-1);
-  const [value, setValue] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<string[]>([]);
-
-  const handleChange = (val: string) => {
-    window.clearTimeout(timeoutRef.current);
-    setValue(val);
-    setData([]);
-
-    if (val.trim().length === 0 || val.includes('@')) {
-      setLoading(false);
+        if (key === "checkbox1" && !value) {
+          emptyInputError[key] = "Please check checkbox1";
+        } else if (key === "checkbox2" && !value) {
+          emptyInputError[key] = "Please check checkbox2";
+        }
+      });
+      setErrorMessages(emptyInputError);
+    } else if (password !== password2) {
+      setErrorMessages(prevState => ({
+        ...prevState,
+        password2: 'Please confirm your password',
+      }));
     } else {
-      setLoading(true);
-      timeoutRef.current = window.setTimeout(() => {
-        setLoading(false);
-        setData(
-          ['gmail.com', 'outlook.com', 'yahoo.com'].map(
-            (provider) => `${val}@${provider}`
-          )
-        );
-      }, 1000);
+      setErrorMessages(defaultRegisterErrorMessage)
+      console.log('submit', form)
     }
-  };
+  }
+  
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target
+    setForm({...form, [name]: value})
+  }
+  
+  useEffect(() => {
+    console.log('form', form);
+    console.log('errorMessages', errorMessages);
+  }, [form, errorMessages]);
 
   return (
     <>
@@ -80,7 +112,7 @@ const Login = () => {
               boxSizing: 'border-box'
             },
             body: {
-              color: COLOR_BLACK
+              color: ConstantStyle.COLOR_BLACK
             },
             a: {
               color: 'inherit!important'
@@ -95,107 +127,118 @@ const Login = () => {
           <Title align="center" size={45} weight={700} mb={65} order={1}>
             SIGN UP
           </Title>
-          <Container size={430}>
-            <Stack spacing={20}>
-              <Input.Wrapper>
-                <Autocomplete
-                  value={value}
-                  data={data}
-                  onChange={handleChange}
-                  rightSection={loading ? <Loader size={16} /> : null}
-                  placeholder="Your email"
-                  radius={5}
-                  size="xl"
-                  label={false}
-                />
-              </Input.Wrapper>
-              <Input.Wrapper>
-                <PasswordInput
-                  placeholder="Your password"
-                  id="your-password"
-                  radius={5}
-                  size="xl"
-                  error="Invalid password"
-                />
-              </Input.Wrapper>
-              <Input.Wrapper>
-                <PasswordInput
-                  placeholder="Re-enter Your password"
-                  id="your-password"
-                  radius={5}
-                  size="xl"
-                />
-              </Input.Wrapper>
-              <Input.Wrapper className={classes.loginIcons}>
-                <Input placeholder="Phone Number" radius={5} size="xl" />
-                <Button
-                  variant="gradient"
-                  color="color-white"
-                  gradient={STYLE_BTN_COLOR}
-                  size="xl"
-                  p={12}
-                  radius={5}
-                  ml={10}
-                >
-                  Verification
-                </Button>
-              </Input.Wrapper>
-              <Input.Wrapper>
-                <Input placeholder="Vertification Code" radius={5} size="xl" />
-              </Input.Wrapper>
-              <Stack spacing={10}>
-                <Box className={classes.registerCheckboxWrap}>
-                  <Checkbox label="Privacy Policy" color="pink" size="md" />
-                  <Anchor
-                    component={Link}
-                    size={16}
-                    underline={true}
-                    className={classes.registerCheckboxMore}
-                    to="/"
-                  >
-                    View Details
-                  </Anchor>
-                </Box>
-                <Box className={classes.registerCheckboxWrap}>
-                  <Checkbox label="Agreement" color="pink" size="md" />
-                  <Anchor
-                    component={Link}
-                    size={16}
-                    underline={true}
-                    className={classes.registerCheckboxMore}
-                    to="/"
-                  >
-                    View Details
-                  </Anchor>
-                </Box>
-              </Stack>
-              <Button
-                variant="gradient"
-                color="color-white"
-                gradient={STYLE_BTN_COLOR}
-                fullWidth
-                size="xl"
-                p={12}
-                radius={5}
-                uppercase
-              >
-                Sign Up
-              </Button>
-              <Button
-                variant="outline"
-                color="pink"
-                fullWidth
-                size="xl"
-                p={12}
-                radius={5}
-                uppercase
-              >
-                <Anchor component={Link} underline={false} to="/Login">
-                  Sign In
-                </Anchor>
-              </Button>
-            </Stack>
-          </Container>
+            <form action="" onSubmit={handleSignUp}>
+              <Container size={430}>
+                  <Stack spacing={20}>
+                    <Input.Wrapper>
+                      <TextInput 
+                        placeholder="Name" 
+                        radius={5} 
+                        size="xl" 
+                        value={form.name} 
+                        onChange={handleInputChange}
+                        name="name" 
+                        error={errorMessages.name}
+                      />
+                    </Input.Wrapper>
+                    <Input.Wrapper>
+                      <TextInput 
+                        placeholder="Email" 
+                        radius={5} 
+                        size="xl" 
+                        value={form.email} 
+                        onChange={handleInputChange}
+                        name="email"
+                        error={errorMessages.email}
+                      />
+                    </Input.Wrapper>
+                    <Input.Wrapper>
+                      <PasswordInput
+                        placeholder="Your password"
+                        radius={5}
+                        size="xl"
+                        name="password"
+                        value={form.password}
+                        onChange={handleInputChange}
+                        defaultValue="secret"
+                          visibilityToggleIcon={({ reveal, size }) =>
+                            reveal ? <IconEyeOff size={size} /> : <IconEyeCheck size={size} />
+                        }
+                        error={errorMessages.password}
+                      />
+                    </Input.Wrapper>
+                    <Input.Wrapper>
+                      <PasswordInput
+                        placeholder="Re-enter Your password"
+                        radius={5}
+                        size="xl"
+                        name="password2"
+                        value={form.password2}
+                        onChange={handleInputChange}
+                        defaultValue="secret"
+                          visibilityToggleIcon={({ reveal, size }) =>
+                            reveal ? <IconEyeOff size={size} /> : <IconEyeCheck size={size} />
+                        }
+                        error={errorMessages.password2}
+                      />
+                    </Input.Wrapper>
+                    <Stack spacing={10}>
+                      <Box className={classes.registerCheckboxWrap}>
+                        <Checkbox label="Privacy Policy" color="pink" size="md" />
+                        <Anchor
+                          component={Link}
+                          size={16}
+                          underline={true}
+                          className={classes.registerCheckboxMore}
+                          to="/"
+                        >
+                          View Details
+                        </Anchor>
+                      </Box>
+                      <Box className={classes.registerCheckboxWrap}>
+                        <Checkbox label="Agreement" color="pink" size="md" />
+                        <Anchor
+                          component={Link}
+                          size={16}
+                          underline={true}
+                          className={classes.registerCheckboxMore}
+                          to="/"
+                          >
+                          View Details
+                        </Anchor>
+                      </Box>
+                    </Stack>
+                    <Button
+                      variant="gradient"
+                      color="color-white"
+                      gradient={ConstantStyle.STYLE_BTN_COLOR}
+                      fullWidth
+                      size="xl"
+                      p={12}
+                      radius={5}
+                      uppercase
+                      type="submit"
+                    >
+                      Sign Up
+                    </Button>
+                    {/* <Button
+                      variant="outline"
+                      color="pink"
+                      fullWidth
+                      size="xl"
+                      p={12}
+                      radius={5}
+                      uppercase
+                    >
+                      <Anchor 
+                        component={Link} underline={false} to="/Login"
+                      >
+                        Sign In
+                      </Anchor>
+                    </Button> */}
+                  </Stack>
+              </Container>
+            </form>
         </Container>
         {/* FOOTER */}
         <LandingFooter />
