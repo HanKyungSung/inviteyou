94bcce1dee89cf9dd79d78f7fc536e1716e95053
Jaydon Lee <hjjaydon.lee@gmail.com>
2023-09-12 18:24:42 -0700

    implemented form value setting

--- invitation-frontend/src/component/Visual3List.tsx
+++ invitation-frontend/src/component/Visual3List.tsx
@@ -17,7 +17,7 @@ interface Visual3ListProps {
   subdomain: string;
 }
 
-interface Participant {
+interface IParticipant {
   name: string;
   participate: string;
   side: string;
@@ -25,19 +25,6 @@ interface Participant {
   note?: string;
   createdAt: string;
   updatedAt: string;
-}
-
-interface IEditableUser {
-  name: string;
-  participate: string;
-  side: string;
-  menu: string;
-  note: string;
-}
-
-interface IEditModal {
-  open: boolean;
-  user: IEditableUser;
 }
 
 const initialConfirmModal = {
@@ -56,6 +43,19 @@ const initEditModal = {
   }
 };
 
+interface IEditableUser {
+  name: string;
+  participate: string;
+  side: string;
+  menu: string;
+  note: string;
+}
+
+interface IEditModal {
+  open: boolean;
+  user: IEditableUser;
+}
+
 interface IConfirmModal {
   open: boolean;
   name: string;
@@ -71,8 +71,65 @@ interface ConfirmDeleteModalProps {
   };
 }
 
+const inputStyle = {
+  wrapper: {
+    'input::placeholder': {
+      margin: '16px 0',
+      fontSize: '16px'
+    },
+    padding: '16px 0 10px 0 !important',
+    borderBottom: '1px solid #ddd !important'
+  },
+  input: {
+    fontSize: '16px'
+  }
+};
+
+const radioGroupStyle = {
+  root: {
+    marginBottom: '20px !important'
+  },
+  required: {
+    color: 'red !important'
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: '16px !important',
+    marginTop: '16px !important'
+  },
+  error: {
+    color: 'red !important',
+    marginTop: '10px !important'
+  }
+};
+
+const radioButtonStyle = {
+  body: {
+    cursor: 'pointer'
+  },
+  icon: {
+    color: 'rgb(180, 152, 133)',
+    width: '12px',
+    height: '12px',
+    top: 'calc(50% - 6px)',
+    left: 'calc(50% - 6px)',
+    cursor: 'pointer'
+  },
+  radio: {
+    cursor: 'pointer',
+    ':checked': {
+      backgroundColor: '#fff',
+      border: '1px solid rgb(180, 152, 133)'
+    },
+    '&::after': {
+      backgroundColor: 'rgb(180, 152, 133)',
+      borderColor: 'rgb(180, 152, 133)'
+    }
+  }
+};
+
 const Visual3List = ({ subdomain }: Visual3ListProps) => {
-  const [participants, setParticipants] = useState<Participant[]>([]);
+  const [participants, setParticipants] = useState<IParticipant[]>([]);
   const [confirmModal, setConfirmModal] =
     useState<IConfirmModal>(initialConfirmModal);
   const [editModal, setEditModal] = useState<IEditModal>(initEditModal);
@@ -230,11 +287,53 @@ interface IEditModalProps {
   getParticipantList: () => void;
   user: IEditableUser;
   subdomain: string;
-  participants: Participant[];
+  participants: IParticipant[];
+}
+
+interface IEditModalErrors {
+  nameError: string;
+  nameExistsError: string;
 }
 
 const EditModal = (props: IEditModalProps) => {
-  const { opened, setModalOpen, user, subdomain } = props;
+  const {
+    opened,
+    setModalOpen,
+    getParticipantList,
+    user,
+    subdomain,
+    participants
+  } = props;
+
+  const [editedUser, setEditedUser] = useState<IEditableUser>({ ...user });
+
+  const [errors] = useState<IEditModalErrors>({
+    nameError: 'Name can not be empty',
+    nameExistsError: 'Entered name already exists'
+  });
+
+  const filteredParticipant = participants.filter(
+    (participant: IParticipant) => participant.name !== user.name
+  );
+
+  const foundParticipant = filteredParticipant.find(
+    (participant: IParticipant) => participant.name === editedUser.name
+  );
+
+  useEffect(() => {
+    setEditedUser({ ...user });
+  }, [user]);
+
+  console.log(editedUser);
+  const nameError = () => {
+    if (editedUser.name === '') {
+      return errors.nameError;
+    } else if (foundParticipant) {
+      return errors.nameExistsError;
+    } else {
+      return '';
+    }
+  };
 
   return (
     <Modal
@@ -244,9 +343,131 @@ const EditModal = (props: IEditModalProps) => {
       title={`UPDATE THE PARTICIPATE ${user.name}`}
       size="xl"
     >
-      <Container>
-        <Visual3RSVPForm subdomain={subdomain} />
-      </Container>
+      <form>
+        <Input.Wrapper
+          required
+          id="edit-modal-name"
+          label="NAME"
+          error={nameError()}
+          styles={{
+            root: {
+              marginBottom: '10px'
+            }
+          }}
+        >
+          <Input
+            placeholder="Name"
+            value={editedUser.name}
+            onChange={(e: React.FormEvent<HTMLInputElement>) => {
+              setEditedUser({
+                ...editedUser,
+                name: e.currentTarget.value
+              });
+            }}
+            styles={inputStyle}
+          />
+          <Radio.Group
+            withAsterisk
+            value={editedUser.participate}
+            onChange={(e) => {
+              setEditedUser({
+                ...editedUser,
+                participate: e
+              });
+            }}
+            name="participate"
+            label="PARTICIPATE"
+            styles={radioGroupStyle}
+          >
+            <Radio value="yes" label="Yes" styles={radioButtonStyle} />
+            <Radio value="no" label="No" styles={radioButtonStyle} />
+          </Radio.Group>
+          <Radio.Group
+            label="INVITED FROM"
+            styles={radioGroupStyle}
+            withAsterisk
+            value={editedUser.side}
+            onChange={(e) => {
+              setEditedUser({
+                ...editedUser,
+                side: e
+              });
+            }}
+          >
+            <Radio
+              name="INVITED FROM"
+              label="Bride Side"
+              value="bride"
+              styles={radioButtonStyle}
+            />
+            <Radio
+              name="INVITED FROM"
+              label="Groom Side"
+              value="groom"
+              styles={radioButtonStyle}
+            />
+            <Radio
+              name="INVITED FROM"
+              label="Both Side"
+              value="both"
+              styles={radioButtonStyle}
+            />
+          </Radio.Group>
+          <Radio.Group
+            label="MENU"
+            value={editedUser.menu}
+            onChange={(e) => {
+              setEditedUser({
+                ...editedUser,
+                menu: e
+              });
+            }}
+            styles={radioGroupStyle}
+            withAsterisk
+          >
+            <Radio
+              name="OPTION 1"
+              label="OPTION 1"
+              value="MENU_OPTION_1"
+              styles={radioButtonStyle}
+            />
+            <Radio
+              name="OPTION 2"
+              label="OPTION 2"
+              value="MENU_OPTION_2"
+              styles={radioButtonStyle}
+            />
+          </Radio.Group>
+          <label>NOTE</label>
+          <textarea
+            name="note"
+            id="note"
+            value={editedUser.note}
+            onChange={(e) =>
+              setEditedUser({
+                ...editedUser,
+                note: e.currentTarget.value
+              })
+            }
+            cols={30}
+            rows={10}
+            placeholder="Please provide us any food restriction you have "
+            style={{
+              width: '100%'
+            }}
+          />
+          <Grid>
+            <Grid.Col span="content">
+              <Button type="submit" color="red">
+                Yes
+              </Button>
+            </Grid.Col>
+            <Grid.Col span="content">
+              <Button onClick={() => setModalOpen(false)}>No</Button>
+            </Grid.Col>
+          </Grid>
+        </Input.Wrapper>
+      </form>
     </Modal>
   );
 };
